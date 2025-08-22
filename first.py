d0e07cb761db6314f582126cc40ad1eb475b07a2
Swapnil Bhong <swapnilbhong1627@gmail.com>
2025-08-22 13:49:45 +0530

    final code review done

--- first.py
+++ first.py
@@ -11,21 +11,6 @@ os.makedirs(data_dir1, exist_ok=True)
 hadoop_home = os.path.abspath("hadoop")   # <-- absolute path
 os.makedirs(os.path.join(hadoop_home, "bin"), exist_ok=True)
 
-urls_and_paths = {
-    "https://raw.githubusercontent.com/saiadityaus1/SparkCore1/master/test.txt":os.path.join(data_dir, "test.txt"),
-    "https://github.com/saiadityaus1/SparkCore1/raw/master/winutils.exe":os.path.join(hadoop_home, "bin", "winutils.exe"),
-    "https://github.com/saiadityaus1/SparkCore1/raw/master/hadoop.dll":os.path.join(hadoop_home, "bin", "hadoop.dll")
-}
-
-# Create an unverified SSL context
-ssl_context = ssl._create_unverified_context()
-
-for url, path in urls_and_paths.items():
-    # Use the unverified context with urlopen
-    with urllib.request.urlopen(url, context=ssl_context) as response, open(path, 'wb') as out_file:
-        data = response.read()
-        out_file.write(data)
-import os, urllib.request, ssl; ssl_context = ssl._create_unverified_context(); [open(path, 'wb').write(urllib.request.urlopen(url, context=ssl_context).read()) for url, path in { "https://github.com/saiadityaus1/test1/raw/main/df.csv": "df.csv", "https://github.com/saiadityaus1/test1/raw/main/df1.csv": "df1.csv", "https://github.com/saiadityaus1/test1/raw/main/dt.txt": "dt.txt", "https://github.com/saiadityaus1/test1/raw/main/file1.txt": "file1.txt", "https://github.com/saiadityaus1/test1/raw/main/file2.txt": "file2.txt", "https://github.com/saiadityaus1/test1/raw/main/file3.txt": "file3.txt", "https://github.com/saiadityaus1/test1/raw/main/file4.json": "file4.json", "https://github.com/saiadityaus1/test1/raw/main/file5.parquet": "file5.parquet", "https://github.com/saiadityaus1/test1/raw/main/file6": "file6", "https://github.com/saiadityaus1/test1/raw/main/prod.csv": "prod.csv", "https://raw.githubusercontent.com/saiadityaus1/test1/refs/heads/main/state.txt": "state.txt", "https://github.com/saiadityaus1/test1/raw/main/usdata.csv": "usdata.csv", "https://github.com/saiadityaus1/SparkCore1/raw/refs/heads/master/data.orc": "data.orc", "https://github.com/saiadityaus1/test1/raw/main/usdata.csv": "usdata.csv", "https://raw.githubusercontent.com/saiadityaus1/SparkCore1/refs/heads/master/rm.json": "rm.json"}.items()]
 
 # ======================================================================================
 
