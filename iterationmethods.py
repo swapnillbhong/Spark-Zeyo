301544e20719ea88b024ef55d1f2e9e468a87898
Swapnil Bhong <swapnilbhong1627@gmail.com>
2025-08-25 13:25:00 +0530

    pratice

---
+++ iterationmethods.py
@@ -0,0 +1,62 @@
+import os
+import urllib.request
+import ssl
+
+data_dir = "data"
+os.makedirs(data_dir, exist_ok=True)
+
+data_dir1 = "hadoop/bin"
+os.makedirs(data_dir1, exist_ok=True)
+
+hadoop_home = os.path.abspath("hadoop")   # <-- absolute path
+os.makedirs(os.path.join(hadoop_home, "bin"), exist_ok=True)
+
+
+# ======================================================================================
+
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+import sys
+import os
+import urllib.request
+import ssl
+
+python_path = sys.executable
+os.environ['PYSPARK_PYTHON'] = python_path
+os.environ['HADOOP_HOME'] = hadoop_home
+os.environ['JAVA_HOME'] = r'C:\Users\Swapnil Bhong\.jdks\corretto-1.8.0_452'        #  <----- 🔴JAVA PATH🔴
+######################🔴🔴🔴################################
+
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.datastax.spark:spark-cassandra-connector_2.12:3.5.1 pyspark-shell'
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-avro_2.12:3.5.4 pyspark-shell'
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.4 pyspark-shell'
+
+
+conf = SparkConf().setAppName("pyspark").setMaster("local[*]").set("spark.driver.host","localhost").set("spark.default.parallelism", "1")
+sc = SparkContext(conf=conf)
+
+spark = SparkSession.builder.getOrCreate()
+
+
+##################🔴🔴🔴🔴🔴🔴 -> DONT TOUCH ABOVE CODE -- TYPE BELOW ####################################
+
+lis = [ 1, 2, 3, 4 ]
+print("======== raw list ======")
+print()
+print(lis)   # input data
+
+rddlis= sc.parallelize(lis)
+print("======== rddlis ======")
+print()
+print(rddlis.collect())   # input data
+
+filrdd = rddlis.filter( lambda  x : x > 2)
+print("======== filrdd ======")
+print()
+print(filrdd.collect())   # input data
+
+filin = rddlis.filter( lambda x  : x <= 2)
+print("======== filin ======")
+print()
+print(filin.collect())   # input data
