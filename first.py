0342c3ac6f7d11660690d5ad9cd84b08def6eedd
Swapnil Bhong <swapnilbhong1627@gmail.com>
2025-09-06 19:12:27 +0530

    class work

--- first.py
+++ first.py
@@ -142,4 +142,53 @@ leftanti  = cust.join(prod, ["id"], "leftanti")
 leftanti.show()
 
 
+source_rdd = spark.sparkContext.parallelize([
+    (1, "A"),
+    (2, "B"),
+    (3, "C"),
+    (4, "D")
+],1)
+
+target_rdd = spark.sparkContext.parallelize([
+    (1, "A"),
+    (2, "B"),
+    (4, "X"),
+    (5, "F")
+],2)
+
+# Convert RDDs to DataFrames using toDF()
+df1 = source_rdd.toDF(["id", "name"])
+df2 = target_rdd.toDF(["id", "name1"])
+
+df1.show()
+df2.show()
+
+
+joindf = df1.join(df2, ["id"], "full" )
+joindf.show()
+
+
+
+comdf = joindf.withColumn("comment" , expr("case when name=name1 then 'match'  else 'mismatch' end"))
+comdf.show()
+
+
+fildf = comdf.filter(" comment !=  'match'   ")
+fildf.show()
+
+
+exprdf = fildf.withColumn("comment", expr("""
+                                                    case
+                                                    when  name1  is null then 'New in Source'
+                                                    when  name   is null then 'New in Target'
+                                                    else comment
+                                                    end
+
+                                                """))
+exprdf.show()
+
+
+finaldf =  exprdf.drop("name","name1")
+finaldf.show()
+
 
