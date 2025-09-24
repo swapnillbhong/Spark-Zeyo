6baa78e2e7f42ef556c95952119ba2b6437342ce
Swapnil Bhong <swapnilbhong1627@gmail.com>
2025-09-24 12:27:00 +0530

    senario 2

---
+++ Senario2.py
@@ -0,0 +1,66 @@
+import os
+import urllib.request
+import ssl
+
+data_dir = "data"
+os.makedirs(data_dir, exist_ok=True)
+
+data_dir1 = "hadoop/bin"
+os.makedirs(data_dir1, exist_ok=True)
+
+hadoop_home = os.path.abspath("hadoop")   # <-- absolute path
+os.makedirs(os.path.join(hadoop_home, "bin"), exist_ok=True)
+
+
+# ======================================================================================
+
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+import sys
+import os
+import urllib.request
+import ssl
+
+python_path = sys.executable
+os.environ['PYSPARK_PYTHON'] = python_path
+os.environ['HADOOP_HOME'] = hadoop_home
+os.environ['JAVA_HOME'] = r'C:\Users\Swapnil Bhong\.jdks\corretto-1.8.0_452'        #  <----- 🔴JAVA PATH🔴
+######################🔴🔴🔴################################
+
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.datastax.spark:spark-cassandra-connector_2.12:3.5.1 pyspark-shell'
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-avro_2.12:3.5.4 pyspark-shell'
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.4 pyspark-shell'
+
+
+conf = SparkConf().setAppName("pyspark").setMaster("local[*]").set("spark.driver.host","localhost").set("spark.default.parallelism", "1")
+sc = SparkContext(conf=conf)
+
+spark = SparkSession.builder.getOrCreate()
+
+
+##################🔴🔴🔴🔴🔴🔴 -> DONT TOUCH ABOVE CODE -- TYPE BELOW ####################################
+
+data = [
+    (1, "1-Jan", "Ordered"),
+    (1, "2-Jan", "dispatched"),
+    (1, "3-Jan", "dispatched"),
+    (1, "4-Jan", "Shipped"),
+    (1, "5-Jan", "Shipped"),
+    (1, "6-Jan", "Delivered"),
+    (2, "1-Jan", "Ordered"),
+    (2, "2-Jan", "dispatched"),
+    (2, "3-Jan", "shipped")]
+myschema = ["orderid","statusdate","status"]
+df = spark.createDataFrame(data,schema=myschema)
+df.show()
+
+finaldf = df.withColumn("status",expr("status == dispatch"))
+finaldf.show()
+
+
+
+
+
+
+
