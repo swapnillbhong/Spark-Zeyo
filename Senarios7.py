412611f34a94ac989420841521b683cd238cb254
Swapnil Bhong <swapnilbhong1627@gmail.com>
2025-09-26 12:25:22 +0530

    senarios 7 complete

---
+++ Senarios7.py
@@ -0,0 +1,69 @@
+import os
+import urllib.request
+import ssl
+
+data_dir = "data"
+os.makedirs(data_dir, exist_ok=True)
+
+data_dir1 = "hadoop/bin"
+os.makedirs(data_dir1, exist_ok=True)
+
+hadoop_home = os.path.abspath("hadoop")   # <-- absolute path
+os.makedirs(os.path.join(hadoop_home, "bin"), exist_ok=True)
+
+
+# ======================================================================================
+
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+import sys
+import os
+import urllib.request
+import ssl
+
+python_path = sys.executable
+os.environ['PYSPARK_PYTHON'] = python_path
+os.environ['HADOOP_HOME'] = hadoop_home
+os.environ['JAVA_HOME'] = r'C:\Users\Swapnil Bhong\.jdks\corretto-1.8.0_452'        #  <----- 🔴JAVA PATH🔴
+######################🔴🔴🔴################################
+
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.datastax.spark:spark-cassandra-connector_2.12:3.5.1 pyspark-shell'
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-avro_2.12:3.5.4 pyspark-shell'
+#os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.4 pyspark-shell'
+
+
+conf = SparkConf().setAppName("pyspark").setMaster("local[*]").set("spark.driver.host","localhost").set("spark.default.parallelism", "1")
+sc = SparkContext(conf=conf)
+
+spark = SparkSession.builder.getOrCreate()
+
+
+##################🔴🔴🔴🔴🔴🔴 -> DONT TOUCH ABOVE CODE -- TYPE BELOW ####################################
+
+from pyspark.sql.functions import *
+from pyspark.sql.window import *
+
+data = [
+    (1, 100, 2010, 25, 5000),
+    (2, 100, 2011, 16, 5000),
+    (3, 100, 2012, 8, 5000),
+    (4, 200, 2010, 10, 9000),
+    (5, 200, 2011, 15, 9000),
+    (6, 200, 2012, 20, 7000),
+    (7, 300, 2010, 20, 7000),
+    (8, 300, 2011, 18, 7000),
+    (9, 300, 2012, 20, 7000)
+]
+myschema = ["sale_id", "product_id", "year", "quantity", "price"]
+df = spark.createDataFrame(data, schema=myschema)
+df.show()
+
+
+win = Window.partitionBy("year").orderBy(col("quantity").desc())
+
+rankdf = df.withColumn("rank", dense_rank().over(win))
+rankdf.show()
+
+finaldf = rankdf.filter(col("rank")== 1).drop("rank").orderBy("sale_id")
+finaldf.show()
